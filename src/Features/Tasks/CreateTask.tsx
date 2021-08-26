21534768a83277b6cd4710440d739b18b8997f4b
Arif Sanaullah <ak7896023@gmail.com>
2021-08-26 07:53:32 +0500

    fix crash on android

--- src/Features/Tasks/CreateTask.tsx
+++ src/Features/Tasks/CreateTask.tsx
@@ -1,112 +1,121 @@
-import React, { useState } from "react";
-import { View, ScrollView } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { addTask } from "./TasksSlice";
-import { RootState } from "../../store";
-import { withTheme, TextInput, IconButton, Text } from "react-native-paper";
-import ListSelector from "../SelectLists/ListSelector";
-import { updateActiveCatagory } from "../Lists/ListsSlice";
+import React, { useState } from 'react';
+import { View, ScrollView } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTask } from './TasksSlice';
+import { RootState } from '../../store';
+import { withTheme, TextInput, IconButton, Text } from 'react-native-paper';
+import ListSelector from '../SelectLists/ListSelector';
+import { updateActiveCatagory } from '../Lists/ListsSlice';
 
 function CreateTask(props: any) {
-  const listData = useSelector((state: RootState) => state.lists);
-  const [taskInput, setTaskInput] = useState("");
-  const [list, setList] = useState(listData.selectedList);
-  const dispatch = useDispatch();
-  const [displayListsSelector, setDisplayListsSelector] = useState(false);
+    const listData = useSelector((state: RootState) => state.lists);
+    const [taskInput, setTaskInput] = useState('');
+    const [list, setList] = useState(listData.selectedList);
+    const dispatch = useDispatch();
+    const [displayListsSelector, setDisplayListsSelector] = useState(false);
 
-  const handleCreatetask = (task: string) => {
-    setTaskInput(task);
-  };
+    const handleCreatetask = (task: string) => {
+        setTaskInput(task);
+    };
 
-  const onSelected = (list: any) => {
-    setList(list);
-    setDisplayListsSelector(false);
-  };
+    const onSelected = (list: any) => {
+        setList(list);
+        setDisplayListsSelector(false);
+    };
 
-  const task = (
-    <View
-      style={{
-        width: "100%",
-        backgroundColor: props.theme.colors.background,
-      }}
-    >
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <View style={{ flexGrow: 1, margin: 5 }}>
-          <TextInput
-            theme={props.theme}
-            dense={true}
-            placeholder="enter a task"
-            onChangeText={handleCreatetask}
-            value={taskInput}
-          ></TextInput>
-        </View>
-
-        <IconButton
-          icon="arrow-right-circle-outline"
-          color={props.theme.colors.accent}
-          {...props}
-          size={30}
-          onPress={() => {
-            dispatch(addTask({ name: taskInput, onList: list }));
-            dispatch(updateActiveCatagory(list));
-          }}
-        />
-      </View>
-      <View style={{ flexDirection: "row" }}>
+    const task = (
         <View
-          style={{
-            margin: "5px",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "20%",
-          }}
-        >
-          <IconButton
-            icon="view-list-outline"
-            color={props.theme.colors.accent}
-            {...props}
-            size={30}
-            onPress={() => {
-              setDisplayListsSelector(!displayListsSelector);
+            style={{
+                width: '100%',
+                backgroundColor: props.theme.colors.background,
             }}
-          ></IconButton>
+        >
+            <View
+                style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <View style={{ flexGrow: 1, margin: 5 }}>
+                    <TextInput
+                        theme={props.theme}
+                        dense={true}
+                        placeholder="enter a task"
+                        onChangeText={handleCreatetask}
+                        value={taskInput}
+                    ></TextInput>
+                </View>
 
-          <Text numberOfLines={2} style={{ color: props.theme.colors.accent }}>
-            {list.name}
-          </Text>
+                <IconButton
+                    icon="arrow-right-circle-outline"
+                    color={props.theme.colors.accent}
+                    {...props}
+                    size={30}
+                    onPress={() => {
+                        dispatch(addTask({ name: taskInput, onList: list }));
+                        dispatch(updateActiveCatagory(list));
+                    }}
+                />
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+                <View
+                    style={{
+                        margin: 5,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '20%',
+                    }}
+                >
+                    <IconButton
+                        icon="view-list-outline"
+                        color={props.theme.colors.accent}
+                        {...props}
+                        size={30}
+                        onPress={() => {
+                            setDisplayListsSelector(!displayListsSelector);
+                        }}
+                    ></IconButton>
+
+                    <Text
+                        numberOfLines={2}
+                        style={{ color: props.theme.colors.accent }}
+                    >
+                        {list.name}
+                    </Text>
+                </View>
+            </View>
         </View>
-      </View>
-    </View>
-  );
-  return (
-    <>
-      {displayListsSelector == true ? (
-        <View
-          style={{ flex: 1, backgroundColor: props.theme.colors.background }}
-        >
-          <ScrollView>
-            <ListSelector onSelected={onSelected} theme={props.theme} />
-          </ScrollView>
-          {task}
-        </View>
-      ) : (
-        <View
-          style={{
-            width: "100%",
-            backgroundColor: props.theme.colors.background,
-          }}
-        >
-          {task}
-        </View>
-      )}
-    </>
-  );
+    );
+    return (
+        <>
+            {displayListsSelector == true ? (
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: props.theme.colors.background,
+                    }}
+                >
+                    <ScrollView>
+                        <ListSelector
+                            onSelected={onSelected}
+                            theme={props.theme}
+                        />
+                    </ScrollView>
+                    {task}
+                </View>
+            ) : (
+                <View
+                    style={{
+                        width: '100%',
+                        backgroundColor: props.theme.colors.background,
+                    }}
+                >
+                    {task}
+                </View>
+            )}
+        </>
+    );
 }
 
 export default withTheme(CreateTask);
--- src/Features/Tasks/EditTask.tsx
+++ src/Features/Tasks/EditTask.tsx
@@ -1,115 +1,127 @@
-import React, { useState, useEffect } from "react";
-import { ScrollView, View } from "react-native";
-import { useDispatch } from "react-redux";
-import { updateTask } from "./TasksSlice";
-import ListSelector from "../SelectLists/ListSelector";
-import { withTheme, TextInput, IconButton, Text } from "react-native-paper";
-import { updateActiveCatagory } from "../Lists/ListsSlice";
-import { updateAppTitle } from "../AppSlice";
+import React, { useState, useEffect } from 'react';
+import { ScrollView, View } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { updateTask } from './TasksSlice';
+import ListSelector from '../SelectLists/ListSelector';
+import { withTheme, TextInput, IconButton, Text } from 'react-native-paper';
+import { updateActiveCatagory } from '../Lists/ListsSlice';
+import { updateAppTitle } from '../AppSlice';
 
 function EditTask(props: any) {
-  const [taskInput, setTaskInput] = useState(props.task.name || "");
-  const dispatch = useDispatch();
-  const [displayListsSelector, setDisplayListsSelector] = useState(false);
-  const [list, setList] = useState(props.task.list);
+    const [taskInput, setTaskInput] = useState(props.task.name || '');
+    const dispatch = useDispatch();
+    const [displayListsSelector, setDisplayListsSelector] = useState(false);
+    const [list, setList] = useState(props.task.list);
 
-  const handleCreatetask = (task: string) => {
-    setTaskInput(task);
-  };
+    const handleCreatetask = (task: string) => {
+        setTaskInput(task);
+    };
 
-  const onSelected = (list: any) => {
-    setList(list);
-    setDisplayListsSelector(false);
-  };
+    const onSelected = (list: any) => {
+        setList(list);
+        setDisplayListsSelector(false);
+    };
 
-  const editTask = (
-    <View
-      style={{
-        width: "100%",
-        backgroundColor: props.theme.colors.background,
-      }}
-    >
-      <View
-        style={{
-          flexDirection: "row",
-        }}
-      >
-        <View style={{ flexGrow: 1 }}>
-          <TextInput
-            theme={props.theme}
-            dense={true}
-            placeholder="edit a task"
-            onChangeText={handleCreatetask}
-            value={taskInput}
-          ></TextInput>
-        </View>
-
-        <IconButton
-          icon="arrow-right-circle-outline"
-          color={props.theme.colors.accent}
-          size={30}
-          onPress={() => {
-            dispatch(
-              updateTask({
-                name: taskInput,
-                id: props.task.id,
-                list: list,
-              })
-            );
-            dispatch(
-              updateActiveCatagory({
-                name: list.name,
-                id: list.id,
-                editable: list.editable,
-              })
-            );
-            dispatch(updateAppTitle(taskInput));
-          }}
-        />
-      </View>
-
-      <View
-        style={{
-          margin: "5px",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "20%",
-        }}
-      >
-        <IconButton
-          icon="view-list-outline"
-          color={props.theme.colors.accent}
-          size={30}
-          onPress={() => {
-            setDisplayListsSelector(!displayListsSelector);
-          }}
-        />
-        <Text numberOfLines={2} style={{ color: props.theme.colors.accent }}>
-          {list.name}
-        </Text>
-      </View>
-    </View>
-  );
-  return (
-    <>
-      {displayListsSelector == true ? (
+    const editTask = (
         <View
-          style={{ flex: 1, backgroundColor: props.theme.colors.background }}
+            style={{
+                width: '100%',
+                backgroundColor: props.theme.colors.background,
+            }}
         >
-          <ScrollView>
-            <ListSelector onSelected={onSelected} theme={props.theme} />
-          </ScrollView>
-          {editTask}
+            <View
+                style={{
+                    flexDirection: 'row',
+                }}
+            >
+                <View style={{ flexGrow: 1 }}>
+                    <TextInput
+                        theme={props.theme}
+                        dense={true}
+                        placeholder="edit a task"
+                        onChangeText={handleCreatetask}
+                        value={taskInput}
+                    ></TextInput>
+                </View>
+
+                <IconButton
+                    icon="arrow-right-circle-outline"
+                    color={props.theme.colors.accent}
+                    size={30}
+                    onPress={() => {
+                        dispatch(
+                            updateTask({
+                                name: taskInput,
+                                id: props.task.id,
+                                list: list,
+                            })
+                        );
+                        dispatch(
+                            updateActiveCatagory({
+                                name: list.name,
+                                id: list.id,
+                                editable: list.editable,
+                            })
+                        );
+                        dispatch(updateAppTitle(taskInput));
+                    }}
+                />
+            </View>
+
+            <View
+                style={{
+                    margin: 5,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '20%',
+                }}
+            >
+                <IconButton
+                    icon="view-list-outline"
+                    color={props.theme.colors.accent}
+                    size={30}
+                    onPress={() => {
+                        setDisplayListsSelector(!displayListsSelector);
+                    }}
+                />
+                <Text
+                    numberOfLines={2}
+                    style={{ color: props.theme.colors.accent }}
+                >
+                    {list.name}
+                </Text>
+            </View>
         </View>
-      ) : (
-        <View
-          style={{ flex: 1, backgroundColor: props.theme.colors.background }}
-        >
-          {editTask}
-        </View>
-      )}
-    </>
-  );
+    );
+    return (
+        <>
+            {displayListsSelector == true ? (
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: props.theme.colors.background,
+                    }}
+                >
+                    <ScrollView>
+                        <ListSelector
+                            onSelected={onSelected}
+                            theme={props.theme}
+                        />
+                    </ScrollView>
+                    {editTask}
+                </View>
+            ) : (
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: props.theme.colors.background,
+                    }}
+                >
+                    {editTask}
+                </View>
+            )}
+        </>
+    );
 }
 
 export default withTheme(EditTask);
